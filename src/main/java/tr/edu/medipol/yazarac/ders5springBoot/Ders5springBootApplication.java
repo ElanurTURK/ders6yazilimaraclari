cfc1f71e21b11a46d13f57ad5a2c424d6559298c
bprlab <bprlab@bprlab-iMac-7.local>
2019-03-30 14:02:04 +0300

    first commit

---
+++ src/main/java/tr/edu/medipol/yazarac/ders5springBoot/Ders5springBootApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazarac.ders5springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders5springBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders5springBootApplication.class, args);
+	}
+
+}
